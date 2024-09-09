86d5b1eb21aa94da748013d3db9eede350e19796
Chimezie Enyinnaya <meziemichael@gmail.com>
2024-09-09 21:49:40 +0100

    email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasFactory, Notifiable, MustVerifyEmail;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
