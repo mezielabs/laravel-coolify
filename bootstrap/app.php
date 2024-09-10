4fbee3756365e7413cc3f56e8d8772b2f900ded1
Chimezie Enyinnaya <meziemichael@gmail.com>
2024-09-10 10:08:58 +0100

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: ['84.247.135.201']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
