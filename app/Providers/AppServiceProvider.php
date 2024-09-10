e8bc2dc107211d0edf32a97c6805a49ed93cb441
Chimezie Enyinnaya <meziemichael@gmail.com>
2024-09-10 15:10:14 +0100

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if ($this->app->environment('production')) {
-        // URL::forceScheme('https');
-        // };
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        };
     }
 }
